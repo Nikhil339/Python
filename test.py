176bfb04123456ebc8e201b8a63ca5cef91f9ec2
Nikhil <revoor.nikhil@gmail.com>
2024-05-17 20:22:41 +0530

    modified test file

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 print("Now reading the contents of the file: ")
-fobject=open("3.txt","r")
-str = fobject.readline().strip()
+object=open("3.txt","r")
+str = object.readline().strip()
 while str:
     print(str)
-    str = fobject.readline().strip()
-fobject.close()
+    str = object.readline().strip()
+object.close()
