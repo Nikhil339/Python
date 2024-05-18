82bd22d69105e0bf009c263fd81fccf5ff5cfd3a
Nikhil <revoor.nikhil@gmail.com>
2024-05-18 23:28:27 +0530

    completed the 2.1 problem from cs50p

--- cs50p/2_1.py
+++ cs50p/2_1.py
@@ -1,1 +1,13 @@
 camel = input("camelCase: ")
+camells = []
+
+for i in camel:
+    if i.islower():
+        camells.append(i)
+    else:
+        camells.append("_")
+        camells.append(i)
+
+print("snakeCase: ", end="")
+for i in camells:
+    print(i, end="")
