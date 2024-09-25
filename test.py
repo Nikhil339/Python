f50b5632ccd67e730a9567ed45a6c4c2230c9e77
Nikhil <revoor.nikhil@gmail.com>
2024-09-25 15:49:41 +0530

    completed python DA 4

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-Dict = {x: x**2 for x in range(1,16)}
-print(Dict)
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+
+x = zip(list1, list2)
+xl = list(x)
+
+l = []
+
+for i in xl:
+    z = i[0] + i[1]
+    l.append(z)
+
+print(l)
